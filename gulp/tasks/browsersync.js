3ef67a5292a2e5942112cc67473aff6544cca48c
Kevin Simard <kev.simard@gmail.com>
2015-03-25 15:49:37 -0400

    Add watcher to browsersync task

--- gulp/tasks/browsersync.js
+++ gulp/tasks/browsersync.js
@@ -2,8 +2,15 @@
     'use strict';
 
     var gulp = require('gulp'),
+        argv = require('yargs').argv,
         browserSync = require('browser-sync'),
         historyApiFallback = require('connect-history-api-fallback');
+
+    if ( !! argv.watch) {
+        gulp.watch('dev/assets/styles/**/*.css', browserSync.reload);
+        gulp.watch(['dev/index.html', 'dev/app/**/*.tpl'], browserSync.reload);
+        gulp.watch(['dev/app/**/*.js', 'dev/assets/scripts/**/*.js'], browserSync.reload);
+    }
 
     gulp.task('browsersync:serve', function () {
         browserSync({
