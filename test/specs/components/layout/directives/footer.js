7499f5130db13c4c364eee4732afaec3102b664c
Kevin Simard <kev.simard@gmail.com>
2015-04-09 14:37:41 -0400

    Add specs for uiHeader & uiFooter directive

---
+++ test/specs/components/layout/directives/footer.js
@@ -0,0 +1,26 @@
+describe('Directive: uiFooter', function () {
+    var $scope, $compile;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function (_$rootScope_, _$compile_) {
+        $scope = _$rootScope_;
+        $compile = _$compile_;
+    }));
+
+    it('should load uiFooter directive as an attribute', function () {
+        var element = $compile('<ui-footer></ui-footer>')($scope);
+
+        $scope.$digest();
+
+        expect(element.scope).toEqual(jasmine.any(Function));
+    });
+
+    it('should load uiFooter directive as an element', function () {
+        var element = $compile('<div ui-footer></div>')($scope);
+
+        $scope.$digest();
+
+        expect(element.scope).toEqual(jasmine.any(Function));
+    });
+});
---
+++ test/specs/components/layout/directives/header.js
@@ -0,0 +1,26 @@
+describe('Directive: uiHeader', function () {
+    var $scope, $compile;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function (_$rootScope_, _$compile_) {
+        $scope = _$rootScope_;
+        $compile = _$compile_;
+    }));
+
+    it('should load uiHeader directive as an attribute', function () {
+        var element = $compile('<ui-header></ui-header>')($scope);
+
+        $scope.$digest();
+
+        expect(element.scope).toEqual(jasmine.any(Function));
+    });
+
+    it('should load uiHeader directive as an element', function () {
+        var element = $compile('<div ui-header></div>')($scope);
+
+        $scope.$digest();
+
+        expect(element.scope).toEqual(jasmine.any(Function));
+    });
+});
