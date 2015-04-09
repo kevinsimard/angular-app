a5fe663e2595647ba65bd985cf6175d1f8e58b04
Kevin Simard <kev.simard@gmail.com>
2015-04-09 14:38:22 -0400

    Change angular variable name from scope to $scope

--- test/specs/views/index/controllers/index.js
+++ test/specs/views/index/controllers/index.js
@@ -1,11 +1,14 @@
 describe('Controller: index.index', function () {
-    var scope;
+    var $scope;
 
     beforeEach(module('app'));
 
     beforeEach(inject(function (_$rootScope_, _$controller_) {
-        scope = _$rootScope_.$new();
-        _$controller_('index.index', { '$scope': scope });
+        $scope = _$rootScope_.$new();
+
+        _$controller_('index.index', {
+            '$scope': $scope
+        });
     }));
 
     it('should load index.index controller', function () {
