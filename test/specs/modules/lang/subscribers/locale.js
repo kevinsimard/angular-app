9d4689ec5e7a4738efbfd78bf56a2c68d0a05917
Kevin Simard <kev.simard@gmail.com>
2015-04-10 14:00:31 -0400

    Add jasmine specs to cover locale subscriber

---
+++ test/specs/modules/lang/subscribers/locale.js
@@ -0,0 +1,28 @@
+describe('Subscriber: locale', function () {
+    var $scope, $state, translate, state, abstractState;
+
+    beforeEach(module('mlang'));
+    beforeEach(module('app'));
+
+    beforeEach(inject(function (_$injector_, _$rootScope_, _$state_) {
+        $scope = _$rootScope_;
+        $state = _$state_;
+
+        translate = _$injector_.get('mlang.translate');
+        state = { 'name': 'index', 'url': '/', 'views': null };
+        abstractState = { 'abstract': true, 'name': '', 'url': '^', 'views': null };
+    }));
+
+    it('should add a part on $stateChangeSuccess event', function () {
+        $scope.$broadcast('$stateChangeSuccess', state, {}, abstractState, {});
+
+        expect(translate.has('index')).toBeTruthy();
+    });
+
+    it('should remove a part on $stateChangeSuccess event', function () {
+        $scope.$broadcast('$stateChangeSuccess', state, {}, abstractState, {});
+        $scope.$broadcast('$stateChangeSuccess', abstractState, {}, state, {});
+
+        expect(translate.has('index')).toBeFalsy();
+    });
+});
